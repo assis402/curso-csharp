4172f443f9346fbad5af7eb164190aeb05017860
Matheus de Assis <assis4002@gmail.com>
2020-10-26 17:21:06 -0300

    Acrescentando tracos

--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ namespace GitTeste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good Morning!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um Bom dia");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("-----------------------------------------");
 
             Console.ReadLine();
         }
