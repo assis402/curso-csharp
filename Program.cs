46292cf3568e20c4bd3248dc0b3d537cd1750c65
Matheus de Assis <assis4002@gmail.com>
2020-10-26 17:04:41 -0300

    Acrescentando mensagem de tchau

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace GitTeste
             Console.WriteLine("Good Morning!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um Bom dia");
+            Console.WriteLine("Tchau!");
 
             Console.ReadLine();
         }
